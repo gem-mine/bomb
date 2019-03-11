0b245c43f492c4906049ee066725b49024eece76
tom <caolvchong@gmail.com>
2019-03-11 11:47:11 +0800

    终端打印修改

--- app/middleware/logger.ts
+++ app/middleware/logger.ts
@@ -5,12 +5,15 @@ import { info as logger } from '../util/logger'
 // 此中间件记录所有请求日志，注意，错误日志由 exception 中间件记录
 export default async function(ctx: Context, next: () => Promise<any>) {
   const start = Date.now()
+  console.log(`>> ${ctx.method}: ${ctx.url}`)
 
   await next()
   const end = Date.now()
   const time = end - start
   const { req } = ctx
   const { headers, url, method } = req
+
+  console.log(`<< ${ctx.method}: ${ctx.url} ${time}ms`)
 
   logger.info('', {
     data: {
