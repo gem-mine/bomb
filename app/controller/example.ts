1b5173171b9898a46b811241cfa03f51d36044bd
tom <caolvchong@gmail.com>
2019-03-12 12:01:09 +0800

    数据库事务、session、cookie、权限验证

--- app/controller/example.ts
+++ app/controller/example.ts
@@ -1,17 +1,20 @@
-import { Body, Delete, Get, Controller, Param, QueryParam, Post, Put, Render } from '@gem-mine/routing-controllers'
+import { Authorized, Body, Ctx, Delete, Get, Controller, Param, QueryParam, Post, Put, Render } from '@gem-mine/routing-controllers'
 import Example from '../service/example'
 import { toJSON, pagination, LIMIT } from '../util/model'
 
+// 示例
 @Controller('/examples')
 export default class {
-  @Get('/page/home')
+  // 渲染页面
   @Render('example.html')
+  @Get('/page/home')
   async page() {
     return {
       name: 'tom'
     }
   }
 
+  // 从数据库中获取列表数据
   @Get('/')
   async getList(@QueryParam('name') name: string = '', @QueryParam('offset') offset: number = 0, @QueryParam('limit') limit: number = LIMIT) {
     const query = await Example.getList({ name, offset, limit })
@@ -19,6 +22,7 @@ export default class {
     return result
   }
 
+  // 往数据库添加一条数据
   @Post('/')
   async createOne(@Body() { name }: { name: string }) {
     const data = await Example.createOne({ name })
@@ -28,21 +32,100 @@ export default class {
     })
   }
 
+  // 从数据库获取一条数据
   @Get('/:id')
   async getOne(@Param('id') id: number) {
     const data = await Example.getOne(id)
     return toJSON({ data })
   }
 
+  // 从数据库修改一条数据
   @Put('/:id')
   async updateOne(@Param('id') id: number, @Body() { name }: { name: string }) {
     const data = await Example.updateOne(id, { name })
     return toJSON({ data })
   }
 
+  // 从数据库删除一条数据
   @Delete('/:id')
   async deleteOne(@Param('id') id: number) {
     const data = await Example.deleteOne(id)
     return toJSON({ data })
+  }
+
+  // 数据库事务操作
+  @Post('/database/transaction')
+  async transaction(@Body() { index, flag }: { index: number; flag: boolean }) {
+    await Example.transaction(index, flag)
+    return { done: true }
+  }
+
+  // redis 取值
+  @Get('/redis/get/age')
+  async getAgeCache() {
+    const value = await Example.getAgeCache()
+    let age: number | undefined
+    if (value) {
+      age = parseInt(value, 10)
+    }
+    return { value: age }
+  }
+
+  // redis 设值
+  @Post('/redis/set/age')
+  async setAgeCache(@Body() { value }: { value: number }) {
+    await Example.setAgeCache(value)
+    return { done: true }
+  }
+
+  // session set 示例
+  @Get('/session/set')
+  async setSession(@Ctx() ctx) {
+    ctx.session.name = 'poly'
+    // 删除整个 session
+    // ctx.session = null
+    // 删除单个 session 的 key
+    // delete ctx.session.name
+    return {
+      message: '请访问 /examples/session/get 查看'
+    }
+  }
+
+  // session get 示例
+  @Get('/session/get')
+  async getSession(@Ctx() ctx) {
+    return {
+      name: ctx.session.name
+    }
+  }
+
+  // cookie set 示例
+  @Get('/cookie/set')
+  async setCookie(@Ctx() ctx) {
+    // 全局过期时间
+    ctx.cookies.set('token', 'abcdefg')
+    //  自定义过期时间
+    ctx.cookies.set('salt', 'haha', {
+      maxAge: 4 * 1000
+    })
+    return {
+      message: '请访问 /examples/cookie/get 查看'
+    }
+  }
+
+  // cookie get 示例
+  @Get('/cookie/get')
+  async getCookie(@Ctx() ctx) {
+    return {
+      token: ctx.cookies.get('token'),
+      salt: ctx.cookies.get('salt')
+    }
+  }
+
+  // 权限验证示例
+  @Authorized()
+  @Get('/auth/verify')
+  async authVerify(@Ctx() ctx) {
+    return ctx.user
   }
 }
--- app/test/controller/example.test.ts
+++ app/test/controller/example.test.ts
@@ -2,6 +2,9 @@ import app from '../../app'
 import './global'
 import request = require('supertest')
 import faker = require('faker')
+const waitForExpect = require('wait-for-expect')
+
+const instance = app.callback()
 
 interface Example {
   id: number
@@ -12,18 +15,18 @@ describe('test case: example controller', () => {
   let item: Example
   let total: number
 
-  test('page/home', async () => {
-    const response = await request(app.callback()).get('/examples/page/home')
+  test('response page: page/home', async () => {
+    const response = await request(instance).get('/examples/page/home')
 
     const { text } = response
     expect(text.includes('hello, tom'))
   })
 
-  test('createOne', async () => {
+  test('restful: createOne', async () => {
     const n = 10
     for (let i = 0; i < n; i++) {
       const name = faker.name.findName()
-      const response = await request(app.callback())
+      const response = await request(instance)
         .post('/examples')
         .send({
           name
@@ -34,22 +37,22 @@ describe('test case: example controller', () => {
     }
   })
 
-  test('getList', async () => {
-    const { body } = await request(app.callback()).get('/examples')
+  test('restful: getList', async () => {
+    const { body } = await request(instance).get('/examples')
     expect(body.data.length >= 10).toBe(true)
     item = body.data[0]
     total = body.pagination.total
   })
 
-  test('getOne', async () => {
-    const { body } = await request(app.callback()).get(`/examples/${item.id}`)
+  test('restful: getOne', async () => {
+    const { body } = await request(instance).get(`/examples/${item.id}`)
     expect(body.id).toBe(item.id)
     expect(body.name).toBe(item.name)
   })
 
-  test('updateOne', async () => {
+  test('restful: updateOne', async () => {
     const name = faker.name.findName()
-    const { body } = await request(app.callback())
+    const { body } = await request(instance)
       .put(`/examples/${item.id}`)
       .send({
         name
@@ -58,17 +61,17 @@ describe('test case: example controller', () => {
     expect(body.name).toBe(name)
   })
 
-  test('deleteOne', async () => {
-    await request(app.callback()).delete(`/examples/${item.id}`)
+  test('restful: deleteOne', async () => {
+    await request(instance).delete(`/examples/${item.id}`)
     const {
       body: { pagination }
-    } = await request(app.callback()).get('/examples')
+    } = await request(instance).get('/examples')
     expect(pagination.total).toEqual(total - 1)
   })
 
   test('database transaction', async () => {
-    const res1 = await request(app.callback()).get('/examples')
-    const res2 = await request(app.callback())
+    const res1 = await request(instance).get('/examples')
+    const res2 = await request(instance)
       .post(`/examples/database/transaction`)
       .send({
         index: 50,
@@ -76,15 +79,73 @@ describe('test case: example controller', () => {
       })
     expect(res2.body.code).toBe(500)
 
-    const res3 = await request(app.callback()).get('/examples')
+    const res3 = await request(instance).get('/examples')
     expect(res1.body.pagination.total).toEqual(res3.body.pagination.total)
-    const res4 = await request(app.callback())
+    const res4 = await request(instance)
       .post(`/examples/database/transaction`)
       .send({
         index: 60
       })
     expect(res4.body.done).toBe(true)
-    const res5 = await request(app.callback()).get('/examples')
+    const res5 = await request(instance).get('/examples')
     expect(res1.body.pagination.total + 2).toEqual(res5.body.pagination.total)
+  })
+
+  test('session', async () => {
+    // 使用 session、cookie 的需要使用 agent
+    // see: https://github.com/visionmedia/supertest/issues/46
+    const agent = request.agent(instance)
+    await agent.get('/examples/session/set')
+    const res = await agent.get('/examples/session/get')
+    expect(res.body.name).toEqual('poly')
+  })
+
+  test('cookie', async done => {
+    const agent = request.agent(instance)
+    await agent.get('/examples/cookie/set')
+    let salt
+    setTimeout(async () => {
+      const res = await agent.get('/examples/cookie/get')
+      salt = res.body.salt
+      done()
+    }, 2000)
+    await waitForExpect(() => {
+      expect(salt).toEqual('haha')
+    })
+  })
+
+  test('cookie but expired', async done => {
+    const agent = request.agent(instance)
+    await agent.get('/examples/cookie/set')
+
+    let salt
+    setTimeout(async () => {
+      const res = await agent.get('/examples/cookie/get')
+      salt = res.body.salt
+      done()
+    }, 4000)
+    await waitForExpect(() => {
+      expect(salt).toEqual(undefined)
+    })
+  })
+
+  test('authorization decorator', async () => {
+    const res1 = await request(instance).get('/examples/auth/verify')
+    expect(res1.body.code).toEqual(401)
+
+    const res2 = await request(instance).get('/examples/auth/verify').set('authorization', 'it_is_a_mock_token')
+    expect(res2.body.id).toEqual(1000)
+    expect(res2.body.name).toEqual('jerry')
+  })
+
+  test('redis', async () => {
+    const age = Math.ceil(Math.random() * 100)
+    const res1 = await request(instance).post('/examples/redis/set/age').send({
+      value: age
+    })
+    expect(res1.body.done).toEqual(true)
+
+    const res2 = await request(instance).get('/examples/redis/get/age')
+    expect(res2.body.value).toEqual(age)
   })
 })
